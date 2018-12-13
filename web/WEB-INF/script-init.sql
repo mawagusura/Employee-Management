9438af4b367d860d8a205c9621345f842902d491
Lucas Masson <lucmasson97@gmail.com>
2018-12-13 11:14:47 +0100

    Fix name

--- web/WEB-INF/script-init.sql
+++ web/WEB-INF/script-init.sql
@@ -42,7 +42,7 @@ INSERT INTO EMPLOYES(NOM,PRENOM,TELDOM,TELPORT,TELPRO,ADRESSE,CODEPOSTAL,VILLE,E
 ('Curie','Pierre', '0123456789','0606060606','0606060607', '45 rue Nagazaki', '01000','Paris','pcurry@efrei.fr'),
 ('Curie','Marie', '0123456780','0706060607','0706060607', '45 rue Hiroshima', '02000','Paris','mcurry@efrei.fr'),
 ('Chollet','Kilian', '0123456790','0606060888','0606060889', '123 rue du soleil', '03000','Marseille','kchollet@efrei.fr'),
-('Lucas','Amauray', '0123456791','0606060999','0606060990', '131 rue Rinner', '04000','Pointe-à-Pitre','alucas@efrei.fr'),
+('Lucas','Amaury', '0123456791','0606060999','0606060990', '131 rue Rinner', '04000','Pointe-à-Pitre','alucas@efrei.fr'),
 ('Masson','Lucas', '0123456792','0606060000','0606060001', '97 place du 18 juin 1940', '05000','Quimper','lmasson@efrei.fr');
 
 CREATE USER 'adm'@'localhost' IDENTIFIED BY 'adm';
